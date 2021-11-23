09f90cdfe7636096dbb1d005aeab8ba86497cbe9
stranger1989 <highway_300_axe1989@yahoo.co.jp>
2021-11-24 08:42:30 +0900

    Style: Update eslint and prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,27 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   extends: [
     'plugin:shopify/react',
     'plugin:shopify/polaris',
     'plugin:shopify/jest',
     'plugin:shopify/webpack',
+    'plugin:shopify/esnext',
+    'plugin:shopify/typescript',
+    'plugin:shopify/prettier',
+    'prettier',
   ],
   rules: {
     'import/no-unresolved': 'off',
+    'no-console': 'warn',
+    'no-alert': 'warn',
+    'no-process-env': 'off',
+    'babel/object-curly-spacing': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'require-atomic-updates': 'off',
   },
   overrides: [
     {
